72caa4b5c8265b1a6f289b53b33360926b7a580a
Tony Sandoval <tsando0304@gmail.com>
2025-01-03 21:11:48 -0700

    created the goalDriver class

---
+++ src/main/java/GoalDriver.java
@@ -0,0 +1,11 @@
+/*
+ * Author: Tony Sandoval
+ * Description: this is the driver class with the main method
+ * */
+public class GoalDriver {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the goal tracker app! ");
+
+    }
+}
