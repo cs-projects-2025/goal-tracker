cb932ba3e8751bd89cea1a60d02a1d94d9818639
Tony Sandoval <tsando0304@gmail.com>
2025-01-17 13:40:15 -0700

    working and running homeView

---
+++ src/main/java/com/goaltracker/goals/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+/*package com.goaltracker.goals.config;
+
+public class SecurityConfig {
+}*/
---
+++ src/main/java/com/goaltracker/goals/views/HomeView.java
@@ -0,0 +1,13 @@
+package com.goaltracker.goals.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class HomeView extends VerticalLayout {
+
+    public HomeView() {
+        add(new H1("Welcome to the Goal Tracker!"));
+    }
+}
